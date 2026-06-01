c59808ef47e7762cca2305e0c4a84d8c5c404c72
faraaz-14 <faraaz1@my.yorku.ca>
2026-06-01 11:13:11 -0400

    Update Part1.py

--- Part1.py
+++ Part1.py
@@ -1,3 +1,4 @@
 print ("Hello World!");
 print ("This is my first Python program.");
 print ("And I am excited to learn more about programming!");
+print ("Currently, I am testing the waters and I am just checking to see if this is making it's way to GitHub");
